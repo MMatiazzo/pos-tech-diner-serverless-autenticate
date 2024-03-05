d616dbe7a68f702d43eedde826119b51d86a2d96
MMatiazzo <mathmatiazzo@gmail.com>
2024-03-05 18:49:54 -0300

    feat: cognito changes

--- lambda.js
+++ lambda.js
@@ -1,7 +1,44 @@
+
+// Modules, e.g. Webpack:
+var AmazonCognitoIdentity = require('amazon-cognito-identity-js');
+
 export const handler = async (event) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!'),
+  var poolData = {
+    UserPoolId: '', // Your user pool id here
+    ClientId: '', // Your client id here
   };
-  return response;
+
+  var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+  
+  var attributeList = [];
+  
+  var dataEmail = {
+    Name: 'email',
+    Value: 'email@mydomain.com',
+  };
+  
+  var dataCpf = {
+    Name: 'CPF',
+    Value: '04411915090',
+  };
+  var attributeEmail = new AmazonCognitoIdentity.CognitoUserAttribute(dataEmail);
+  var attributeCpf = new AmazonCognitoIdentity.CognitoUserAttribute(
+    dataCpf
+  );
+  
+  attributeList.push(attributeEmail);
+  attributeList.push(attributeCpf);
+  
+  userPool.signUp('usernameTest', 'b{XR{3yfk+=p)^BJ', attributeList, null, function(
+    err,
+    result
+  ) {
+    if (err) {
+      alert(err.message || JSON.stringify(err));
+      return;
+    }
+    var cognitoUser = result.user;
+    console.log('user name is ' + cognitoUser.getUsername());
+  });
+  
 };
