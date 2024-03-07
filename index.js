07bdb0ee3ddde45019f4dde23aafda2b3d1da5bd
Carloto <viniciuscarloto@gmail.com>
2024-03-07 18:33:59 -0300

    feat: change import

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
 // Modules, e.g. Webpack:
 import AmazonCognitoIdentity from 'amazon-cognito-identity-js';
-import { Client } from 'pg'
+import pg from 'pg';
 
 export const handler = async (event) => {
 
-  const client = new Client({ connectionString: "postgresql://postgres_username:postgres_password@rds-pos-tech-diner.cpiuqcs2ov56.us-east-1.rds.amazonaws.com:5432/postechdinerdb" })
+  const client = new pg.Client({ connectionString: "postgresql://postgres_username:postgres_password@rds-pos-tech-diner.cpiuqcs2ov56.us-east-1.rds.amazonaws.com:5432/postechdinerdb" })
   await client.connect()
 
   const res = await client.query('SELECT * FROM produtos')
