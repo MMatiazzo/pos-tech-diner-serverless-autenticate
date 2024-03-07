0d60ba5e62e9a68da0382d8963ccc4592c1a1434
Carloto <viniciuscarloto@gmail.com>
2024-03-07 18:19:26 -0300

    feat: test connecting to postgres

--- index.js
+++ index.js
@@ -1,44 +1,53 @@
 
 // Modules, e.g. Webpack:
 import AmazonCognitoIdentity from 'amazon-cognito-identity-js';
+import { Client } from 'pg'
 
 export const handler = async (event) => {
-  var poolData = {
-    UserPoolId: '', // Your user pool id here
-    ClientId: '', // Your client id here
-  };
 
-  var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+  const client = new Client({ connectionString: "postgresql://postgres_username:postgres_password@rds-pos-tech-diner.cpiuqcs2ov56.us-east-1.rds.amazonaws.com:5432/postechdinerdb" })
+  await client.connect()
 
-  var attributeList = [];
+  const res = await client.query('SELECT * FROM produtos')
+  console.log(res) // Hello world!
+  await client.end()
 
-  var dataEmail = {
-    Name: 'email',
-    Value: 'email@mydomain.com',
-  };
+  // var poolData = {
+  //   UserPoolId: '', // Your user pool id here
+  //   ClientId: '', // Your client id here
+  // };
 
-  var dataCpf = {
-    Name: 'CPF',
-    Value: '04411915090',
-  };
-  var attributeEmail = new AmazonCognitoIdentity.CognitoUserAttribute(dataEmail);
-  var attributeCpf = new AmazonCognitoIdentity.CognitoUserAttribute(
-    dataCpf
-  );
+  // var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
 
-  attributeList.push(attributeEmail);
-  attributeList.push(attributeCpf);
+  // var attributeList = [];
 
-  userPool.signUp('usernameTest', 'b{XR{3yfk+=p)^BJ', attributeList, null, function (
-    err,
-    result
-  ) {
-    if (err) {
-      alert(err.message || JSON.stringify(err));
-      return;
-    }
-    var cognitoUser = result.user;
-    console.log('user name is ' + cognitoUser.getUsername());
-  });
+  // var dataEmail = {
+  //   Name: 'email',
+  //   Value: 'email@mydomain.com',
+  // };
+
+  // var dataCpf = {
+  //   Name: 'CPF',
+  //   Value: '04411915090',
+  // };
+  // var attributeEmail = new AmazonCognitoIdentity.CognitoUserAttribute(dataEmail);
+  // var attributeCpf = new AmazonCognitoIdentity.CognitoUserAttribute(
+  //   dataCpf
+  // );
+
+  // attributeList.push(attributeEmail);
+  // attributeList.push(attributeCpf);
+
+  // userPool.signUp('usernameTest', 'b{XR{3yfk+=p)^BJ', attributeList, null, function (
+  //   err,
+  //   result
+  // ) {
+  //   if (err) {
+  //     alert(err.message || JSON.stringify(err));
+  //     return;
+  //   }
+  //   var cognitoUser = result.user;
+  //   console.log('user name is ' + cognitoUser.getUsername());
+  // });
 
 };
