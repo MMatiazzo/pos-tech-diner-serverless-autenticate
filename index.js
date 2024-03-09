272b92e9a34f36dff6ca010416fd10f0f8e987a1
Carloto <viniciuscarloto@gmail.com>
2024-03-09 15:29:36 -0300

    feat: search by cpf

--- index.js
+++ index.js
@@ -4,52 +4,13 @@ import AmazonCognitoIdentity from 'amazon-cognito-identity-js';
 import pg from 'pg';
 
 export const handler = async (event) => {
+  const { cpf } = event;
 
-  const client = new pg.Client({ connectionString: "postgresql://postgres_username:postgres_password@rds-pos-tech-diner.cpiuqcs2ov56.us-east-1.rds.amazonaws.com:5432/postechdinerdb" })
+  const client = new pg.Client({ connectionString: "postgresql://postgres_username:postgres_password@rds-pos-tech-diner.cpiuqcs2ov56.us-east-1.rds.amazonaws.com:5432/postechdinerdb" });
   await client.connect()
 
-  const res = await client.query('SELECT * FROM clientes')
-  console.log(res)
+  const res = await client.query('SELECT email FROM clientes WHERE cpf=$1', [cpf])
   await client.end()
 
   return res;
-
-  // var poolData = {
-  //   UserPoolId: '', // Your user pool id here
-  //   ClientId: '', // Your client id here
-  // };
-
-  // var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
-
-  // var attributeList = [];
-
-  // var dataEmail = {
-  //   Name: 'email',
-  //   Value: 'email@mydomain.com',
-  // };
-
-  // var dataCpf = {
-  //   Name: 'CPF',
-  //   Value: '04411915090',
-  // };
-  // var attributeEmail = new AmazonCognitoIdentity.CognitoUserAttribute(dataEmail);
-  // var attributeCpf = new AmazonCognitoIdentity.CognitoUserAttribute(
-  //   dataCpf
-  // );
-
-  // attributeList.push(attributeEmail);
-  // attributeList.push(attributeCpf);
-
-  // userPool.signUp('usernameTest', 'b{XR{3yfk+=p)^BJ', attributeList, null, function (
-  //   err,
-  //   result
-  // ) {
-  //   if (err) {
-  //     alert(err.message || JSON.stringify(err));
-  //     return;
-  //   }
-  //   var cognitoUser = result.user;
-  //   console.log('user name is ' + cognitoUser.getUsername());
-  // });
-
 };
