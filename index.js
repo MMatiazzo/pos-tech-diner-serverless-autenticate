7b1c2ae186ecfc7ac8e94426d30b2d9dff227390
Carloto <viniciuscarloto@gmail.com>
2024-03-09 15:34:35 -0300

    feat: get cpf from event body

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import AmazonCognitoIdentity from 'amazon-cognito-identity-js';
 import pg from 'pg';
 
 export const handler = async (event) => {
-  return event
-  const { cpf } = event;
+  const body = JSON.parse(event.body);
+  const { cpf } = body
 
   const client = new pg.Client({ connectionString: "postgresql://postgres_username:postgres_password@rds-pos-tech-diner.cpiuqcs2ov56.us-east-1.rds.amazonaws.com:5432/postechdinerdb" });
   await client.connect()
