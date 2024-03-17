56bf3edce65878914a35d3b3fdb1b8cd15c0a36a
Carloto <viniciuscarloto@gmail.com>
2024-03-17 12:01:15 -0300

    feat: add dot env and variable replace

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 // Modules, e.g. Webpack:
-import AmazonCognitoIdentity from 'amazon-cognito-identity-js';
-import pg from 'pg';
 import axios from 'axios';
+import 'dotenv/config';
+import pg from 'pg';
 
 export const handler = async (event) => {
 
@@ -25,7 +25,7 @@ export const handler = async (event) => {
     const { username, email } = event;
 
     await cognitoApi.post('/', {
-      "ClientId": "278fmimfp7pl48hs52jnctihlg", // trocar por env var
+      "ClientId": process.env.COGNITO_CLIENT_ID,
       "Password": password,
       "Username": username,
       "UserAttributes": [
@@ -42,7 +42,7 @@ export const handler = async (event) => {
   }
 
   // Retrieve username
-  const client = new pg.Client({ connectionString: "postgresql://postgres_username:postgres_password@rds-pos-tech-diner.cpiuqcs2ov56.us-east-1.rds.amazonaws.com:5432/postechdinerdb" });
+  const client = new pg.Client({ connectionString: process.env.POSTGRES_CONNECTION_STRING });
   await client.connect();
 
   const res = await client.query('SELECT nome FROM clientes WHERE cpf=$1', [cpf])
@@ -54,7 +54,7 @@ export const handler = async (event) => {
   console.log('before request',)
   const { data } = await cognitoApi.post('/', {
     "AuthFlow": "USER_PASSWORD_AUTH",
-    "ClientId": "278fmimfp7pl48hs52jnctihlg",
+    "ClientId": process.env.COGNITO_CLIENT_ID,
     "AuthParameters": {
       "USERNAME": username,
       "PASSWORD": password
@@ -69,12 +69,3 @@ export const handler = async (event) => {
 
   return data;
 };
-
-//test
-// await handler({
-//   cpf: "000000000",
-//   password: "X0>d1G3c",
-//   signup: true,
-//   username: "mm",
-//   email: "teste@email.com"
-// });
